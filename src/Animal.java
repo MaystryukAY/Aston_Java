6c0234293d5bb5ad02cd7dfdafcaa2902da50893
MaystryukAY <a.maistriuc@yandex.ru>
2025-06-09 17:29:14 +0500

    Домашняя работа, 1 задание - Иерархия животных. Майстрюк Андрей Юрьевич Aston
    Signed-off-by: MaistryukAY <a.maistriuc@yandex.ru>

--- src/Animal.java
+++ src/Animal.java
@@ -2,7 +2,6 @@ public abstract class Animal {
     private String name;
     private int age;
     private int weight;
-
     public Animal(String name, int age, int weight) {
         this.name = name;
         this.age = age;
